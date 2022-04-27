e22bd90698b7dc42493c64b91e0c9d1e7cb53ae2
imashnake0 <kamaleshpaluru07@gmail.com>
2022-04-27 16:08:27 -0400

    Updated AGP and Gradle
    Updated AGP to 7.1.3 and Gradle to 7.4.2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "7.0.1" apply false
-    id("com.android.library") version "7.0.1" apply false
+    id("com.android.application") version "7.1.3" apply false
+    id("com.android.library") version "7.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.5.20" apply false
 }
 
